fa54e67f86dd57753f3679e45d3b964da3ae4126
Arinze ogbechi <atozbasic@gmail.com>
2022-08-06 14:03:31 +0100

    change on the auth route header(usertoken) to header('usertoken') $ home route rse to res

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,11 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = function (req, res, next) {
-	const token = req.header(usertoken);
+	const token = req.header("usertoken");
 	if (!token) return res.status(400).send("access denied⚡");
 	try {
-		req.User = jwt.verify(token, process.env.TOKEN_SECRET);
+		const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+		req.User = verified;
 		next();
 	} catch (err) {
 		res.status(400).send(" The token didn't match⚡");
--- route/home.js
+++ route/home.js
@@ -2,8 +2,8 @@ const Route = require("express").Router();
 
 const authUser = require("../middleware/auth");
 
-Route.get("/home", authUser, (req, rse) => {
-	res.send("<h1>welcome to home Route</h1>");
+Route.get("/home", authUser, (req, res) => {
+	res.json({ message: "welcome to home Route" });
 });
 
 module.exports = Route;
